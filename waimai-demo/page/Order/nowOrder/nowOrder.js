58e11e958b13d67d3f8aab6db511455aa65fd9e7
xx23855 <xx23855@ly.com>
2017-05-16 16:07:46 +0800

    comment

--- waimai-demo/page/Order/nowOrder/nowOrder.js
+++ waimai-demo/page/Order/nowOrder/nowOrder.js
@@ -1,6 +1,38 @@
 // page/Order/nowOrder/nowOrder.js
 Page({
-  data:{},
+  data:{
+    flag2:5
+  },
+  changeColor11:function(){
+    var that = this;
+    that.setData( {
+      flag2: 1
+    });
+  },
+  changeColor12:function(){
+    var that = this;
+    that.setData( {
+      flag2:2
+    });
+  },
+  changeColor13:function(){
+    var that = this;
+    that.setData( {
+      flag2: 3
+    });
+  },
+  changeColor14:function(){
+    var that = this;
+    that.setData( {
+      flag2:4
+    });
+  },
+  changeColor15:function(){
+    var that = this;
+    that.setData( {
+      flag2: 5
+    });
+  },
   toComment:function () {
     wx.navigateTo({
             url: '../comment/comment'
